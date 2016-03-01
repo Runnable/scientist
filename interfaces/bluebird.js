e12f1d8b1bbf6e3526a393d63ef3742089a5e3d2
Bryan Kendall <bryan@runnable.com>
2016-03-01 12:20:31 -0800

    remove extra comments

--- interfaces/bluebird.js
+++ interfaces/bluebird.js
@@ -1,7 +1,3 @@
 declare module 'bluebird' {
-  // declare function resolve <T> (object?: Promise<T> | T): Promise<T>;
-  // declare var exports: {
-  //   resolve <T> (object?: Promise<T> | T): Promise<T>;
-  // }
   declare var exports: typeof Promise;
 }
